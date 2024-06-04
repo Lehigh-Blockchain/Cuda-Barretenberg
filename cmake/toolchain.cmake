3838cd3469f5cccf991c1d71d0f45ef402ede721
mikeKaufman310 <91042615+mikeKaufman310@users.noreply.github.com>
2024-06-04 10:16:27 -0400

    Update toolchain.cmake

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,5 +1,5 @@
 if(NOT TOOLCHAIN)
-  set(TOOLCHAIN "x86_64-linux-clang" CACHE STRING "Build toolchain." FORCE)
+  set(TOOLCHAIN "x86_64-linux-gcc10" CACHE STRING "Build toolchain." FORCE)
 endif()
 message(STATUS "Toolchain: ${TOOLCHAIN}")
 
